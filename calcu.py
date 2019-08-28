e1c400ff2e979a7249a034d0cf067adc1ba1df8a
Lively-Engels <54520176+Lively-Engels@users.noreply.github.com>
2019-08-28 02:05:22 -0500

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,16 @@
+num1 = input("inserte el primer numero")
+num2 = unput("inserte el segundo numero")
+
+suma = num1+num2
+print "la suma de los numeros es:",suma
+
+resta = num1+num2
+print "la Resga de los numeros es:",resta
+
+mult = num1*num2
+print "la suma de los numeros es:",mult
+
+div = num1/num2
+print "la Resga de los numeros es:",div
+.
+03
